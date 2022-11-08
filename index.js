723ea90b03933c0190e8f2b082e2fde4bde756b9
Dipta04 <diptakarmakar38@gmail.com>
2022-11-08 07:09:16 +0600

    service

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ahr0ub7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+  
+    if(!authHeader)
+    {
+      return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+      if(err)
+      {
+        return res.status(403).send({message: 'Forbidden access'});
+      }
+      req.decoded = decoded;
+      next();
+    })
+  
+  }
+  
+  async function run(){
+    try{
+        const serviceCollection = client.db('kitchen').collection('services');
+
+        app.post('/jwt', (req, res) =>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'})
+            res.send({token})
+      })
+
+      app.get('/services', async(req, res) =>{
+        const query = {}
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+       })
+       
+       app.get('/services/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+      })
+
+    }
+    finally{
+
+    }
+  }
+  run().catch(error => console.error(error));
+
+app.get('/', (req, res) => {
+  res.send('cloud kitchen server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Cloud kitchen server running on ${port}`)
+})
