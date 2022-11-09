d894e6de2a5786313ef2944ce71cfc603dfb1e7b
Dipta04 <diptakarmakar38@gmail.com>
2022-11-09 12:20:56 +0600

    reviews

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ function verifyJWT(req, res, next){
   async function run(){
     try{
         const serviceCollection = client.db('kitchen').collection('services');
+        const reviewCollection = client.db('kitchen').collection('reviews');
 
         app.post('/jwt', (req, res) =>{
             const user = req.body;
@@ -65,6 +66,53 @@ function verifyJWT(req, res, next){
         const service = await serviceCollection.findOne(query);
         res.send(service);
       })
+
+      app.get('/reviews', verifyJWT, async (req, res) => {
+            
+        const decoded = req.decoded;
+        if(decoded.email !== req.query.email)
+        {
+           res.status(403).send({message: 'unauthorized access'})
+        }
+        let query = {};
+
+        if(req.query.email)
+        {
+          query = {
+             email: req.query.email
+          }
+        }
+
+        const cursor = reviewCollection.find(query);
+        const reviews = await cursor.toArray();
+        res.send(reviews);
+    });
+
+    app.post('/reviews', verifyJWT, async(req, res) =>{
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+   })
+
+   app.patch('/reviews/:id', verifyJWT, async(req, res) =>{
+    const id = req.params.id;
+    const status = req.body.status;
+    const query = { _id: ObjectId(id) }
+    const updatedDoc = {
+      $set:{
+          status: status
+      }
+    }
+    const result = await reviewCollection.updateOne(query, updatedDoc);
+    res.send(result);
+   })
+
+   app.delete('/reviews/:id', verifyJWT, async(req, res) =>{
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await reviewCollection.deleteOne(query);
+    res.send(result);
+    })
 
     }
     finally{
